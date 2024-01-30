6597e5fdc88178aa936e51010a3db9f73b245f88
evairx <xxbrazzersxx35@gmail.com>
2024-01-30 20:22:27 -0300

    add comments.js

---
+++ comments.js
@@ -0,0 +1,28 @@
+// Create web server
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.get('/comments', function(req, res) {
+  fs.readFile('comments.json', function(err, data) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+  });
+});
+
+app.post('/comments', function(req, res) {
+  fs.readFile('comments.json', function(err, data) {
+    var comments = JSON.parse(data);
+    comments.push(req.body);
+    fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(comments));
+    });
+  });
+});
+
+app.listen(3000);
+console.log('Server started: http://localhost:3000/');
