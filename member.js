9578c133a501ca11762790d0e67b5bd8d611e921
evairx <xxbrazzersxx35@gmail.com>
2024-01-30 20:19:38 -0300

    add member.js

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    return {
+        members: [
+            {
+                name: 'John Doe',
+                skills: ['JavaScript', 'React', 'Node.js'],
+                experience: '5 years'
+            },
+            {
+                name: 'Jane Smith',
+                skills: ['Python', 'Django', 'SQL'],
+                experience: '3 years'
+            },
+            {
+                name: 'Alex Johnson',
+                skills: ['Java', 'Spring', 'Hibernate'],
+                experience: '7 years'
+            }
+        ],
+        totalMembers: 3
+    };
+}
